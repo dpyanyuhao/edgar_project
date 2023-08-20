477d5f4dc91a05ee4cb4127f03e28feb6f5b1eb7
Yuhao Yan <jerry-yan@hotmail.com>
2023-08-20 23:31:07 +0800

    update plotly

--- data_analyzer/views.py
+++ data_analyzer/views.py
@@ -24,7 +24,18 @@ def dropdown(request):
     if request.method == "POST":
         selected_cik = int(request.POST.get('manager_dropdown'))
         positions = PositionInfo.objects.filter(cik=selected_cik)
-        positions_with_security_info = positions.values('cusip__ticker', 'cusip__name', 'value', 'shares', 'filing_period').order_by('filing_period')
+        positions_with_security_info = positions.values('cusip__ticker', 'cusip__name', 'cusip__sector', 'value', 'shares', 'filing_period').order_by('filing_period')
         context.update({'positions': positions_with_security_info, 'selected_cik': selected_cik})
+
+        import plotly.express as px
+        import pandas as pd
+
+        df = pd.DataFrame(positions_with_security_info)
+        df['filing_period'] = pd.to_datetime(df['filing_period'], format='%d-%b-%Y')
+        print(df['filing_period'].unique())
+
+        fig = px.bar(df, x="filing_period", y="value", color="cusip__name")
+        plot = fig.to_html(full_html=False, default_height=500, default_width=800)
+        context.update({'plot': plot})
 
     return render(request, 'dropdown.html', context)
